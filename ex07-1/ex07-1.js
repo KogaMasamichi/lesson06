126740427b6687c90262608edb158a29e294f824
KogaMasamichi <km.060928@gmail.com>
2021-01-04 23:56:55 +0900

    ex07-1完成

--- ex07-1/ex07-1.js
+++ ex07-1/ex07-1.js
@@ -13,3 +13,16 @@ function splitString(stringToSplit, separator) {
 
     return arrayOfStrings
 }
+
+const weekString = "sun,mon,tue,wed,thu,fri,sat"
+const comma = ","
+
+var tempestString = weekString
+const weekSplit = splitString(tempestString, comma)
+console.log(`一週間は${weekSplit.length}日あります。${weekSplit}です。`)
+
+const monthString = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec"
+var tempestString = monthString
+const monthSprit = splitString(tempestString, comma)
+console.log(`一年は${monthSprit.length}ヶ月あります。${monthSprit}です。`)
+//本来の用途とは違うとは思っています。
