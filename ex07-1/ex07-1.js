37664b468a29273fe8e581b5b400986d7b3017f9
KogaMasamichi <km.060928@gmail.com>
2021-01-06 02:59:05 +0900

    誤字を修正。exportを追加

--- ex07-1/ex07-1.js
+++ ex07-1/ex07-1.js
@@ -1,28 +1,34 @@
+//trimStart trimEnd
+
 const str = " Hello World "
 const trimStr = str.trimStart().trimEnd()
 console.log(str)
 console.log(trimStr)
 
+
+//slice toUpperCase
+
 const slupStr = str.slice(1, 5).toUpperCase()
 console.log(slupStr)
 
 
+//split
 
-function splitString(stringToSplit, separator) {
+export default function splitString(stringToSplit, separator) {
     const arrayOfStrings = stringToSplit.split(separator)
 
     return arrayOfStrings
 }
 
-const weekString = "sun,mon,tue,wed,thu,fri,sat"
+const week = "sun,mon,tue,wed,thu,fri,sat"
 const comma = ","
 
-var tempestString = weekString
-const weekSplit = splitString(tempestString, comma)
+
+const weekSplit = splitString(week, comma)
 console.log(`一週間は${weekSplit.length}日あります。${weekSplit}です。`)
 
-const monthString = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec"
-var tempestString = monthString
-const monthSprit = splitString(tempestString, comma)
-console.log(`一年は${monthSprit.length}ヶ月あります。${monthSprit}です。`)
+const month = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec"
+
+const monthSplit = splitString(month, comma)
+console.log(`一年は${monthSplit.length}ヶ月あります。${monthSplit}です。`)
 //本来の用途とは違うとは思っています。
